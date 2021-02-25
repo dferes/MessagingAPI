92567788909e8d2ec803412160dc14a76e975571
Dylan Feres <dferes23@gmail.com>
2021-02-25 11:38:32 -0800

    removed the ExpressErrors and return json errors instead

--- routes/auth.js
+++ routes/auth.js
@@ -1,56 +1,56 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const User = require('./models/user');
+const User = require('../models/user');
 const ExpressError = require('../expressError');
 const { SECRET_KEY } = require('../config');
 
 const router = new express.Router();
 
-/* POST /login - login: {username, password} => {token}
-   Make sure to update their last-login! */
+
 router.post('/login', async (req, res, next) => {
     try {
         let { username, password } = req.body;
         let user = await User.get(username);
         
-        if (!user.firstName) throw new ExpressError(user.message, 404);
+        if (!user.first_name) return res.status(409).json({ error: `No such username: ${username}`});
 
         let currentUser = new User(username, password, user.firstName, user.lastName, user.phone);
         
         if (await User.authenticate(username, password)) {
             let token = jwt.sign({username}, SECRET_KEY);
-            currentUser.updateLoginTimestamp();
-            return res.json({token});
+            await currentUser.updateLoginTimestamp();
+            return res.json({ token: token });
         }
+        return res.status(409).json({ error: 'Invalid password' });
     }catch(e) {
         return next(e);
     }
 
 })
 
-/** POST /register - register user: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- *
- *  Make sure to update their last-login!
- */
+
+
 router.post("/register", async (req, res, next) => {
     try {
-      let {username, password, first_name, last_name, phone} = req.body;
-      const user = await User.get(username);
+        let {username, password, first_name, last_name, phone} = req.body;
+        const user = await User.get(username);
+        if(!user.message) {
+            return res.status(409).json({ error: `User account with username ${username} already exists` });
+        }
+        const newUser = new User(username, password, first_name, last_name, phone);
+        await newUser.register();
+
+        let token = jwt.sign({username}, SECRET_KEY);
+        await newUser.updateLoginTimestamp();
+
+        return res.status(201).json({ token: token });
+        
       // 409 represents a request that could not be completed due to a conflict 
-      // with the current state of the resource"
-      if(user.rows) throw new ExpressError(`User account with username ${username} already exists`, 409); 
-      const newUser = new User(username, password, first_name, last_name, phone);
-      await newUser.register();
-      let token = jwt.sign({username}, SECRET_KEY);
-      newUser.updateLoginTimestamp();
-      return res.sataut(201).json({token});
+      // with the current state of the resource
+
+    } catch (err) {
+        return next(err);
     }
-  
-    catch (err) {
-      return next(err);
-    }
-  });
+});
 
  module.exports = router;
