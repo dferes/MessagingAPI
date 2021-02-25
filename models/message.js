31c966662dc2ce177984811dab5d6c59c17077fb
Dylan Feres <dferes23@gmail.com>
2021-02-24 22:25:05 -0800

    more descriptive variable names

--- models/message.js
+++ models/message.js
@@ -6,6 +6,21 @@ class Message {
 
   /* register new message -- returns {id, from_username, to_username, body, sent_at} */
   static async create(from_username, to_username, body) {
+    if (!body) {
+      return new ExpressError('Message must contain a body', 404);
+    }
+    const fromUser = await db.query(
+      `SELECT * FROM users WHERE username = $1`, [from_username]);
+    const toUser = await db.query(
+      `SELECT * FROM users WHERE username = $1`, [to_username]);
+    
+    if( toUser.rows.length === 0) {
+      return new ExpressError(`User ${to_username} not found`, 404);
+    }
+    if( fromUser.rows.length === 0) {
+      return new ExpressError(`User ${from_username} not found`, 404);
+    }
+
     const result = await db.query(
         `INSERT INTO messages (from_username, to_username, body, sent_at)
         VALUES ($1, $2, $3, current_timestamp)
@@ -17,6 +32,8 @@ class Message {
 
   /* Update read_at for message */
   static async markRead(id) {
+    if(isNaN(id)) return new ExpressError('id must be an integer', 400);
+
     const result = await db.query(
         `UPDATE messages
         SET read_at = current_timestamp
@@ -25,21 +42,16 @@ class Message {
         [id]);
 
     if (!result.rows[0]) {
-      throw new ExpressError(`No such message: ${id}`, 404);
+      return new ExpressError(`No such message: ${id}`, 404);
     }
 
     return result.rows[0];
   }
 
-  /** Get: get message by id
-   *
-   * returns {id, from_user, to_user, body, sent_at, read_at}
-   *
-   * both to_user and from_user = {username, first_name, last_name, phone}
-   *
-   */
-
+  /* Get: get message by id returns {id, from_user, to_user, body, sent_at, read_at} */
   static async get(id) {
+    if(isNaN(id)) return new ExpressError('id must be an integer', 400);
+
     const result = await db.query(
         `SELECT m.id,
                 m.from_username,
@@ -59,29 +71,27 @@ class Message {
           WHERE m.id = $1`,
         [id]);
 
-    let m = result.rows[0];
+    let message = result.rows[0];
 
-    if (!m) {
-      throw new ExpressError(`No such message: ${id}`, 404);
-    }
+    if (!message) return new ExpressError(`No such message: ${id}`, 404);
 
     return {
-      id: m.id,
+      id: message.id,
       from_user: {
-        username: m.from_username,
-        first_name: m.from_first_name,
-        last_name: m.from_last_name,
-        phone: m.from_phone,
+        username: message.from_username,
+        first_name: message.from_first_name,
+        last_name: message.from_last_name,
+        phone: message.from_phone,
       },
       to_user: {
-        username: m.to_username,
-        first_name: m.to_first_name,
-        last_name: m.to_last_name,
-        phone: m.to_phone,
+        username: message.to_username,
+        first_name: message.to_first_name,
+        last_name: message.to_last_name,
+        phone: message.to_phone,
       },
-      body: m.body,
-      sent_at: m.sent_at,
-      read_at: m.read_at,
+      body: message.body,
+      sent_at: message.sent_at,
+      read_at: message.read_at,
     };
   }
 }
