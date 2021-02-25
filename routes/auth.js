21a9b69f539c9e819c6c249f71f8b2684206803a
Dylan Feres <dferes23@gmail.com>
2021-02-24 23:36:10 -0800

    added 2 routes for authorizatin of user credentials

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('./models/user');
-const Message = require('./models/message');
 const ExpressError = require('../expressError');
 const { SECRET_KEY } = require('../config');
 
@@ -13,16 +12,18 @@ router.post('/login', async (req, res, next) => {
     try {
         let { username, password } = req.body;
         let user = await User.get(username);
-        if (user.error) {
-            continue; // come back to this...
-        }
+        
+        if (!user.firstName) throw new ExpressError(user.message, 404);
+
+        let currentUser = new User(username, password, user.firstName, user.lastName, user.phone);
+        
         if (await User.authenticate(username, password)) {
             let token = jwt.sign({username}, SECRET_KEY);
-            User.updateLoginTimestamp(username);
+            currentUser.updateLoginTimestamp();
             return res.json({token});
         }
     }catch(e) {
-
+        return next(e);
     }
 
 })
@@ -33,5 +34,23 @@ router.post('/login', async (req, res, next) => {
  *
  *  Make sure to update their last-login!
  */
+router.post("/register", async (req, res, next) => {
+    try {
+      let {username, password, first_name, last_name, phone} = req.body;
+      const user = await User.get(username);
+      // 409 represents a request that could not be completed due to a conflict 
+      // with the current state of the resource"
+      if(user.rows) throw new ExpressError(`User account with username ${username} already exists`, 409); 
+      const newUser = new User(username, password, first_name, last_name, phone);
+      await newUser.register();
+      let token = jwt.sign({username}, SECRET_KEY);
+      newUser.updateLoginTimestamp();
+      return res.sataut(201).json({token});
+    }
+  
+    catch (err) {
+      return next(err);
+    }
+  });
 
  module.exports = router;
