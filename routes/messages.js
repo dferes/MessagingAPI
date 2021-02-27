04133347cd1761cdab5170746d195e1081b9ad24
Dylan Feres <dferes23@gmail.com>
2021-02-26 23:02:49 -0800

    added first route

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const Message = require('../models/message');
 const ExpressError = require('../expressError');
+const { authenticateJWT, ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth');
 
 const router = new express.Router();
 
@@ -14,10 +15,22 @@ const router = new express.Router();
  *               read_at,
  *               from_user: {username, first_name, last_name, phone},
  *               to_user: {username, first_name, last_name, phone}}
- *
- * Make sure that the currently-logged-in users is either the to or from user.
- *
- **/
+ */
+router.get('/:id', ensureLoggedIn, async (req, res, next) => {
+  if (! req.params.id) return res.status(400).json({ error: 'id paramater must be provided' });
+  try {  
+    const message = await Message.get(req.params.id);
+    if (message.message) return res.status(message.status).json({ error: message.message });
+
+    if ( ![message.from_user.username, message.to_user.username].includes(req.user.user.username) ) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+    
+    return res.status(200).json({ message: message });
+  } catch(e) {
+     return next(e); 
+  }
+});
 
 
 /** POST / - post message.
